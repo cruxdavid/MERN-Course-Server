12d1221d76cb9e96db616dc6963e28ea40d3b6d1
David Cruz <d.crux10@gmail.com>
2021-06-06 21:51:11 -0600

    configed passport and mongo+mongoose

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
+const keys = require("./src/config/keys");
+const mongoose = require("mongoose");
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+require("./src/models/User");
+require("./src/services/passport");
+
+// Init express
 const app = express();
 
-app.get('/', (req, res)=> {
-  res.send({bye: 'buddy'});
-});
+//Connect routes with express
+require("./src/routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
